a44a37efb9d8e1e6145c3b26db5f199aa1aee4b6
llVeraVerall <Sladkovavera7@gmail.com>
2022-11-21 15:47:44 +0300

    first commit

---
+++ src/components/BoardComponents.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useEffect, useState } from 'react';
+import { Board } from '../models/Board';
+import { Cell } from '../models/Cell';
+import { CellComponent } from './CellComponent';
+
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+export const BoardComponents: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (
+      selectedCell &&
+      selectedCell !== cell &&
+      selectedCell.figure?.canMove(cell)
+    ) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    } else {
+      setSelectedCell(cell);
+    }
+  }
+
+  useEffect(() => {
+    highlighCells();
+  }, [selectedCell]);
+
+  function highlighCells() {
+    board.highlighCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="board">
+      {board.cells.map((row, index) => (
+        <React.Fragment key={index}>
+          {row.map((cell) => (
+            <CellComponent
+              cell={cell}
+              click={click}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+              }
+            />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
